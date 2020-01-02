1a4fb201877b2cc2d21b4e4d09ca26b3fc7ce5c9
Calvin Zheng <calvinzheng@exosite.com>
2020-01-02 23:18:56 +0800

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  plugins: ["prettier"],
+  extends: [
+    "eslint:recommended",
+    "prettier"
+  ],
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint"
+      ]
+    }
+  ]
+};
