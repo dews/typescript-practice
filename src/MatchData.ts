ac507ef1c92a3ee4679851e54939aa742cfbdccd
Calvin Zheng <calvinzheng@exosite.com>
2020-01-19 17:41:14 +0800

    Remove src

--- src/MatchData.ts
+++
@@ -1,2 +0,0 @@
-import { MatchResult } from "./MatchResult";
-export type MatchData = [Date, string, string, number, number, MatchResult, string];
--- src/reportTargets/ConsoleReport.ts
+++
@@ -1,9 +0,0 @@
-import { OutputTarget } from "../Summary";
-
-export class ConsoleReport implements OutputTarget {
-  constructor() {}
-
-  print(report: string): void {
-    console.log(report);
-  }
-}
