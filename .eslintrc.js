3d0e973cc4df62aa3e7ad5efe7b6dc967e0956cf
Calvin Zheng <calvinzheng@exosite.com>
2020-01-27 23:23:15 +0800

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,37 @@
 module.exports = {
   root: true,
-  files: ["*.js"],
-  plugins: ["prettier"],
-  extends: [
-    "eslint:recommended",
-    "prettier"
-  ],
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    ecmaVersion: 2019,
-  },
   overrides: [
+    {
+      files: ["*.js"],
+      plugins: ["prettier"],
+      extends: [
+        "eslint:recommended",
+        "plugin:prettier/recommended"],
+      env: {
+        browser: true,
+        node: true
+      },
+      parserOptions: {
+        ecmaVersion: 2019
+      },
+    },
     {
       files: ["*.ts", "*.tsx"],
       parser: "@typescript-eslint/parser",
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"]
+      },
       plugins: ["@typescript-eslint"],
       extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "prettier/@typescript-eslint"
-      ]
+      ],
+      rules: {
+        "prettier/prettier": ["error", { singleQuote: true, semi: false }]
+      }
     }
   ]
 };
