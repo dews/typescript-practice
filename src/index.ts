fa809964ed286b331a964592fa0e994e9fb9088e
Calvin Zheng <calvinzheng@exosite.com>
2020-03-22 00:17:17 +0800

    Finish "Advanced Express and TS Integration"

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
+import morgan from 'morgan'
+import { AppRouter } from './AppRouter'
+import './controllers/LoginController'
+import './controllers/RootController'
 
 const app = express()
-
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>
-        <h1>Hi</h1>
-    </div>
-    `)
+app.use(morgan('dev'))
+app.use(cookieSession({ name: 'yyy', keys: ['sdfsglek'], maxAge: 60 * 1000 }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next) {
+  req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
+  next()
 })
+app.use(AppRouter.getInstance())
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
