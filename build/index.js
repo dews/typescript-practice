6a2068ee6d87613092cadf4c7f38aebe81c4e58b
Calvin Zheng <calvinzheng@exosite.com>
2020-03-28 17:22:33 +0800

    Revert "Finish "Advanced Express and TS Integration""
    This reverts commit fa809964ed286b331a964592fa0e994e9fb9088e.

--- build/index.js
+++ build/index.js
@@ -3,23 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
-var cookie_session_1 = __importDefault(require("cookie-session"));
-var morgan_1 = __importDefault(require("morgan"));
-var AppRouter_1 = require("./AppRouter");
-require("./controllers/LoginController");
-require("./controllers/RootController");
-var app = express_1.default();
-app.use(morgan_1.default('dev'));
-app.use(cookie_session_1.default({ name: 'yyy', keys: ['sdfsglek'], maxAge: 60 * 1000 }));
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-    req.session.nowInMinutes = Math.floor(Date.now() / 60e3);
-    next();
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/', (req, res) => {
+    res.send(`
+    <div>
+        <h1>Hi</h1>
+    </div>
+    `);
 });
-app.use(AppRouter_1.AppRouter.getInstance());
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cookieSession from 'cookie-session'
-import morgan from 'morgan'
-import { AppRouter } from './AppRouter'
-import './controllers/LoginController'
-import './controllers/RootController'
+import express, { Request, Response } from 'express'
 
 const app = express()
-app.use(morgan('dev'))
-app.use(cookieSession({ name: 'yyy', keys: ['sdfsglek'], maxAge: 60 * 1000 }))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(function(req, res, next) {
-  req.session.nowInMinutes = Math.floor(Date.now() / 60e3)
-  next()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+        <h1>Hi</h1>
+    </div>
+    `)
 })
-app.use(AppRouter.getInstance())
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
